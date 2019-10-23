c63dc5bfb6e326311567dbfe979e4e5b7b59220a
marimeli <marimel.casas@gmail.com>
2019-10-23 15:41:03 -0500

    functions

---
+++ functions.js
@@ -0,0 +1,29 @@
+function getSum(num1, num2) {
+    return num1 + num2;
+}
+;
+//console.log(getSum(1,4));
+var mySum = function (num1, num2) {
+    if (typeof num1 == 'string') {
+        num1 = parseInt(num1);
+    }
+    if (typeof num2 == 'string') {
+        num2 = parseInt(num2);
+    }
+    return num1 + num2;
+};
+console.log(mySum('3', 5));
+// ? symbol makes the parameter optional
+function getName(firstname, lastname) {
+    if (lastname == undefined) {
+        return firstname;
+    }
+    return firstname + ' ' + lastname;
+}
+;
+console.log(getName('John'));
+//void returns anything at all
+function myVoid() {
+    return;
+}
+;
