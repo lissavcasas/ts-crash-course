51881c8ed53dc617d86ca22c70fb8c1d57ba3e22
marimeli <marimel.casas@gmail.com>
2019-10-23 15:53:04 -0500

    interfaces

---
+++ interfaces.js
@@ -0,0 +1,19 @@
+/* function showToDo(todo: {title:string, text:string}){
+  console.log(todo.title + ' ' + todo.text);
+};
+
+let myTodo = {
+  title: 'Trash',
+  text: 'Take out trash'
+}
+
+showToDo(myTodo); */
+function showToDo(todo) {
+    console.log(todo.title + ' ' + todo.text);
+}
+;
+var myTodo = {
+    title: 'Trash',
+    text: 'Take out trash'
+};
+showToDo(myTodo);
